2480c2a4e8a260257bd06073191a9506070312a7
joshua <joshua04dcosta@gmail.com>
2025-08-23 13:07:38 +0530

    example addition lib

--- rosdl/cli.py
+++ rosdl/cli.py
@@ -1,6 +1,20 @@
 import click
+from rosdl.addition import add
 
-@click.command()
+@click.group()
+def cli():
+    """rosdl - Research Oriented Smart Data Library"""
+    pass
+
+@cli.command()
 def hello():
     """Say Hello from rosdl"""
     click.echo("Hello, World from rosdl!")
+
+@cli.command()
+@click.argument("a", type=int)
+@click.argument("b", type=int)
+def add(a, b):
+    """Add two numbers"""
+    result = add(a, b)
+    click.echo(f"The sum of {a} and {b} is {result}")
